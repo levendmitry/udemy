524163d6e27eefcbc86cf49aa1631909fad1eece
leven.dmitry <leven.dmitry@gmail.com>
2023-09-20 19:24:05 +0300

    draft

--- draftPractise/practise.js
+++
@@ -1,9 +0,0 @@
-function isPangram(string) {
-  let checkString = string;
-  const uniqueLetters = Array.from(new Set(checkString.replace(/\s/g, "").toLowerCase()));
-  console.log(uniqueLetters.length);
-  return uniqueLetters.length >= 27;
-}
-
-isPangram("The quick brown fox jumps over the lazy dog");
-
