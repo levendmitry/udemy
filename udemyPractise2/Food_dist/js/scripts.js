06cfa404ca809df4f83d87735888b2672fcb3e33
Leven Dmitry <leven.dmitry@gmail.com>
2023-09-24 13:37:53 +0300

    add timer

--- udemyPractise2/Food_dist/js/scripts.js
+++ udemyPractise2/Food_dist/js/scripts.js
@@ -1,5 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
 
+    //Tabs
+
     const tabs = document.querySelectorAll(".tabheader__item"),
           tabsContent = document.querySelectorAll(".tabcontent"),
           tabsParent = document.querySelector(".tabheader__items");
@@ -37,6 +39,59 @@ window.addEventListener("DOMContentLoaded", () => {
             );
         }
     });
+
+    //Timer
+
+    const deadline = '2023-10-4';
+
+    function getTimeRemaining(endTime) {
+        const timeDifference = Date.parse(endTime) - Date.parse(new Date()),
+              days = Math.floor(timeDifference / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((timeDifference / (1000 * 60 * 60)) % 24),
+              minutes = Math.floor((timeDifference / 1000 / 60) % 60),
+              seconds = Math.floor((timeDifference / 1000) % 60);
+
+        return {
+            "total": timeDifference,
+            days,
+            hours,
+            minutes,
+            seconds,
+        };
+    }
+
+    function setZero(num) {
+       return num >= 0 && num < 10 ? `0${num}` : num;
+    }
+
+    function setClock(selector, endTime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector("#days"),
+              hours = timer.querySelector("#hours"),
+              minutes = timer.querySelector("#minutes"),
+              seconds = timer.querySelector("#seconds"),
+              timeInterval = setInterval(updateClock, 1000);
+        
+        updateClock();
+
+        function updateClock() {
+            const timeLeft = getTimeRemaining(endTime);
+
+            days.innerHTML = setZero(timeLeft.days);
+            hours.innerHTML = setZero(timeLeft.hours);
+            minutes.innerHTML = setZero(timeLeft.minutes);
+            seconds.innerHTML = setZero(timeLeft.seconds);
+
+            if (timeLeft.total <= 0) {
+                clearInterval(timeInterval);
+            } 
+        }
+    }
+
+    setClock(".timer", deadline);
+
+
+
 
 
 });
