40e964fc523f4b45d9b2e9d5ac682f195ef1740f
Leven Dmitry <leven.dmitry@gmail.com>
2023-10-18 15:22:40 +0300

    json server added

--- draftPractise/draft/script.js
+++ draftPractise/draft/script.js
@@ -1,54 +1,16 @@
 'use strict';
 
-// console.log("request data");
+// Array methods
 
-// const request = new Promise(function(resolve, reject){
-//     setTimeout(() => {
-//         console.log("....");
-    
-//         const product = {
-//             name: "TV",
-//             price: 2000,
-//         };
+const names = ["Ivan", "Dmitrii", "Alexey", "Gena"];
 
-//         resolve(product);
-//     }, 2000);
-// });
-
-// request.then((product) => {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             product.status = "ordered";
-//             resolve(product);
-//         }, 2000);
-//     });
-// }).then(data => {
-//     data.modify = true;
-//     return data;
-// }).then((data) => {
-//     console.log(data);
-// }).catch(() => {
-//     console.error("error");
-// }).finally(() => {
-//     console.log("thats all folks");
-// });
-
-
-const test = function(time) {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve();
-        }, time);
-    });
-};
-
-// test(1000).then(() => console.log("1000ms"));
-// test(2000).then(() => console.log("2000ms"));
-
-Promise.all([test(1000), test(2000)]).then(() => {
-    console.log("all is fine");
+const shortNames = names.filter(function(name) {
+    return name.length < 5;
 });
 
-Promise.race([test(1000), test(2000)]).then(() => {
-    console.log("all is fine");
-});
+console.log(shortNames);
+
+
+
+
+
